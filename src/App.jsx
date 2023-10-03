084147deb53ac47b2d056a1f95efd51012e64310
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-03 17:52:57 +0200

    added and styled header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import TopBar from './components/topBar/TopBar';
+import Header from './components/Header/Header';
 
 
 function App() {
 
   return (
-    <TopBar />
+    <>
+      <TopBar />
+      <Header />
+    </>
   )
 }
 
